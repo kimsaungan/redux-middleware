7202d53881c839d898b998bfe914c4cd79b24806
ksa <kimsaungan@gihub.com>
2022-09-15 16:38:01 +0900

    redux-thunk 적용하기 increase(), decrease()dp setTimeout()을 적용하여 thunk 함수인 increaseAsync(), decreaseAsync() 생성

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import rootReducer from './modules';
 import myLogger from "./middlewares/myLogger";
 import logger from 'redux-logger';
 import {composeWithDevTools} from "redux-devtools-extension";
+import ReduxThunk from 'redux-thunk';
 
 
 // applyMiddleware()를 통해 미들웨어를 적용시킴
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(logger))
+    //logger를 사용하는 경우, logger가 가장 마지막에 와야한다.
+    composeWithDevTools(applyMiddleware(ReduxThunk, logger))
 );  // 여러 개의 미들웨어를 적용할 수 있습니다.
 
 ReactDOM.render(
